d285977768885393dd0cfec7cb12ce3bf8539c74
kureiketohka_local <aka.siro.sinsen@gmail.com>
2024-05-03 18:30:51 +0900

    20240503

---
+++ template.js
@@ -0,0 +1,8 @@
+function header() {
+    document.write('<header><img src="./home_img/city_rogo.png" alt="市のマーク" width=6.5%><b>&nbsp;東揚羽市</b></header>');
+}
+
+
+function footer() {
+    document.write('<footer>▶このホームページの内容はすべてフィクションで、実際の自治体、行事、慣習と一切関係ありません。</footer>')
+}

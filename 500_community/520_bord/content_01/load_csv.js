3ae152c1bb49f4355992e8bd1e8eee47035b3472
kureiketohka_local <aka.siro.sinsen@gmail.com>
2024-06-08 20:15:20 +0900

    20240608

--- 500_community/520_bord/content_01/load_csv.js
+++ 500_community/520_bord/content_01/load_csv.js
@@ -1,13 +1,29 @@
-const outputElement = document.getElementById('test_csv');
+const output_csv = document.getElementById('test_csv');
 
-function getCsvData(dataPath) {
- const request = new XMLHttpRequest();
- request.addEventListener('load', (event) => {
-  const response = event.target.responseText;
-  outputElement.innerHTML = response;
- });
- request.open('GET', dataPath, true);
- request.send();
+function csv_data(dataPath) {
+	const request = new XMLHttpRequest(); // HTTPでファイルを読み込む
+	request.addEventListener('load', (event) => { // ロードさせ実行
+		const response = event.target.responseText; // 受け取ったテキストを返す
+		csv_array(response); //csv_arrayの関数を実行
+	});
+	request.open('GET', dataPath, true); // csvのパスを指定
+	request.send();
 }
 
-getCsvData('test.csv');
+function csv_array(data) {
+	const dataArray = []; //配列を用意
+	const dataString = data.split('\n'); //改行で分割
+	for (let i = 0; i < dataString.length; i++) { //あるだけループ
+		dataArray[i] = dataString[i].split(',');
+	}
+	let insertElement = ''; //テーブルタグに表示する用の変数
+	dataArray.forEach((element) => { //配列の中身を表示
+		insertElement += '<tr>';
+		element.forEach((childElement) => {
+			insertElement += `<td>${childElement}</td>`
+		});
+		insertElement += '</tr>';
+	});
+	output_csv.innerHTML = insertElement; // 表示
+}
+csv_data('test.csv'); // csvのパス

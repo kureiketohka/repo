49c3747b1c6e52fed65e74777a3d93bbf2036ef7
kureiketohka_local <aka.siro.sinsen@gmail.com>
2024-06-23 11:54:45 +0900

    20246023

--- 500_community/520_bord/bord_top.js
+++ 500_community/520_bord/bord_top.js
@@ -3,6 +3,8 @@ function gate() {
     var UserInput = prompt("パスワードを入力して下さい:","");
     if (UserInput=='Answer') {
         location.href = "./521_bord/ura_bord_top.html";
-    } 
+    } else {
+        alert("違います");
+    }
     
 }
